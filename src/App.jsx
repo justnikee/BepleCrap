ef5e1787a12720fa9a1ec7e03419eaf712d70be0
justnikee <hk5561111212@gmail.com>
2022-12-11 23:13:44 +0530

    smooth scroll and gsap

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,32 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Header.components/Homepage.components/HomePage";
 import WorkPage from "./components/Header.components/WorkPage.jsx/WorkPage";
 import ContactUs from "./components/Header.components/contactus.component/ContactUs";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  });
+
+  lenis.on("scroll", ({ scroll, limit, velocity, direction, progress }) => {
+    console.log({ scroll, limit, velocity, direction, progress });
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
   return (
     <Router>
       <div className="app">
--- src/components/Header.components/Homepage.components/NewSections/HeroSection.jsx
+++ src/components/Header.components/Homepage.components/NewSections/HeroSection.jsx
@@ -1,84 +1,96 @@
 import React, { useEffect } from "react";
 import ProfilePicture from "../../../../assets/pp.svg";
-import Love from "../../../../assets/love.svg";
-import Death from "../../../../assets/death.svg";
-import Robot from "../../../../assets/robot.svg";
-
+import styled from "styled-components";
 import gsap from "gsap";
 
 const HeroSection = () => {
   useEffect(() => {
-    const tl = gsap.timeline();
-    tl.from(".anime span", {
-      y: 200,
-      delay: 0.6,
-      duration: 1.1,
-      stagger: 0.3,
-    }).to(".icon_rotate", 5, {
-      rotation: 360,
-
-      repeat: -1,
+    const tl = gsap.timeline({
+      defaults: { ease: "Power4.inOut", duration: 2 },
     });
+    tl.to(".anime", {
+      "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+      opacity: 1,
+      y: 0,
+      duration: 2,
+    })
+      .to(
+        ".sub_text",
+        {
+          "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+          opacity: 1,
+          duration: 1.5,
+          y: 0,
+        },
+        "-=1.5"
+      )
+      .to(
+        ".button",
+        {
+          "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+          opacity: 1,
+          duration: "1",
+          y: 0,
+        },
+        "-=1.2"
+      );
   });
 
   return (
-    <div className="flex h-screen ">
-      <div className="flex flex-col gap-5 border-r-2 border-b-2 border-black bg-[#ffffff] justify-center items-start p-10 w-1/2">
-        <h1 className="anime text-black text-6xl w-full font-[Syne] overflow-hidden">
-          <span className="anim_text font-[Syne] inline-block overflow-hidden">
-            Hii Im Nikhil
-          </span>
-          <br />
-          <span className="anim_text font-[Syne] inline-block overflow-hidden">
-            & I Build
-          </span>
-          <br />
-          <span className="anim_text font-[Syne] inline-block">
-            Things For Web
-          </span>
-        </h1>
-        <p className="text-xl w-1/2 font-[MuseoModerno]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-          possimus eius placeat veritatis explicabo ipsam debitis in nulla, est
-          soluta!
-        </p>
-        <button className="bg-[#ffc900] px-16 py-3 border-r-black shadow uppercase font-bold ">
-          contact now
-        </button>
-      </div>
-      <div className="flex flex-col relative w-1/2 bg-[#ffffff] border-b-2 border-black justify-center items-center">
-        <div className="p-20">
-          <img className="h-full w-96" src={ProfilePicture} alt="" />
+    <>
+      <div className="flex h-screen ">
+        <div className="flex flex-col gap-5 border-r-2 border-b-2 border-black bg-[#ffffff] justify-center items-start p-10 w-1/2">
+          <Heading className="anime text-black text-6xl w-full font-[Syne] overflow-hidden">
+            <span className="anim_text font-[Syne] inline-block">
+              Hii Im Nikhil
+            </span>
+            <br />
+            <span className="anim_text font-[Syne] inline-block">
+              & I Build
+            </span>
+            <br />
+            <span className="anim_text font-[Syne] inline-block">
+              Things For Web
+            </span>
+          </Heading>
+          <SubHeading className="text-xl w-1/2 font-[MuseoModerno] sub_text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            possimus eius placeat veritatis explicabo ipsam debitis in nulla,
+            est soluta!
+          </SubHeading>
+          <Button className="button bg-[#ffc900] px-16 py-3 border-r-black shadow uppercase font-bold ">
+            contact now
+          </Button>
         </div>
-        <span>
-          <img
-            className=" icon_rotate w-1/4 absolute top-1/2 right-12"
-            src={Love}
-            alt=""
-          />
-        </span>
-        <span>
-          <img
-            className="w-1/4 absolute bottom-1/4 left-6"
-            src={Death}
-            alt=""
-          />
-        </span>
-        <div>
-          <img
-            className="icon_rotate w-1/4 absolute top-16 left-10"
-            src={Robot}
-            alt=""
-          />
+        <div className="flex flex-col relative w-1/2 bg-[#ffffff] border-b-2 border-black justify-center items-center">
+          <div className="p-20">
+            <img className="h-full w-96" src={ProfilePicture} alt="" />
+          </div>
         </div>
-        {/* <img className="absolute bottom-12 left-6 icon" src={icons} alt="" />
-        <img className="absolute right-4 bottom-10" src={icon2} alt="" />
-        <button className="absolute bottom-10 right-1/3 bg-orange-500 px-8 uppercase py-2 shadow">
-          Nikhil
-        </button> */}
       </div>
-    </div>
+      <div className="h-screen w-screen bg-slate-600 flex justify-center items-center">
+        <h1>section 2</h1>
+      </div>
+    </>
   );
 };
+
+const Button = styled.button`
+  clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);
+  opacity: 0;
+  transform: translateY(100px);
+`;
+
+const SubHeading = styled.p`
+  clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);
+  opacity: 0;
+  transform: translateY(100px);
+`;
+
+const Heading = styled.h1`
+  clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0% 100%);
+  opacity: 0;
+  transform: translateY(100px);
+`;
 
 export default HeroSection;
