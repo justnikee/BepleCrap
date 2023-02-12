8f3f16cfc6f33cd3542eba169ec3481473ae5ad1
justnikee <hk5561111212@gmail.com>
2023-02-12 22:57:31 +0530

    fixed issues

--- src/components/Header.components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return <div>this is fkin footer baby</div>;
-};
-
-export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
-import "./Fonts/10 Canopee/Canopee.otf";
-import "./Fonts/16 Dahlia/Dahlia Bold Condensed.otf";
-import "./Fonts/34 Rang/RANG2.otf";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
